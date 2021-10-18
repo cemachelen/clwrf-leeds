62fdff03a37b4fdc7df6db566084371a19ea8e96
cemachelen <h.l.burns@leeds.ac.uk>
2021-10-18 19:30:02 +0100

    :wrench: added build scripts and config file for basic set up

---
+++ buildscripts/linknchdf5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Quick script for patcheling in the netcdf libraries
+#
+module load patchelf
+
+for BIX in $(find ./* -maxdepth 1 \
+                 -type f )
+    do
+      # add hdf5 / netcdf lib directories to rpath if required:
+      ldd ${BIX} | grep -q hdf5 >& /dev/null
+      if [ "${?}" = "0" ] ; then
+        BIX_RPATH=$(patchelf --print-rpath ${BIX})
+        patchelf --set-rpath "${HDF5_HOME}/lib:${BIX_RPATH}" \
+          ${BIX}
+      fi
+      ldd ${BIX} | grep -q netcdf >& /dev/null
+      if [ "${?}" = "0" ] ; then
+        BIX_RPATH=$(patchelf --print-rpath ${BIX})
+        patchelf --set-rpath "${NETCDF_HOME}/lib:${BIX_RPATH}" \
+          ${BIX}
+      fi
+    done
